b8f9c874d5a468088e1e41547c53c201107fc71d
Pritam sen <spartian.sen738@gmail.com>
2018-05-06 01:12:01 +0530

    word counter added and also collapsables will close when a new word is displayed

--- src/component/dashboard.js
+++ src/component/dashboard.js
@@ -4,56 +4,83 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      head: '',
       index: 0,
-      body: '',
+      x: [],
       data: []
     };
   }
   componentDidUpdate = () => {
-    if (this.state.data.length == 0) {
+    var x = [];
+    if (this.state.data.length === 0) {
       let word = this.props.data[this.state.index];
       word = word.split(":");
-      this.setState({head: word[0], body: word[1], data: this.props.data});
+      x.push(<ul key="1" className="collapsible">
+        <li>
+          <div className="collapsible-header">
+            <i className="material-icons">done</i>{word[0]}</div>
+          <div className="collapsible-body">
+            <span>{word[1]}</span>
+          </div>
+        </li>
+      </ul>)
+      this.setState({x: x, data: this.props.data});
     }
   }
   next = () => {
+    $('.collapsible').collapsible('close', 0);
+    var x = [];
     var i = this.state.index + 1;
     let word = this.props.data[i];
-    console.log(word)
     word = word.split(":");
-    this.setState({head: word[0], body: word[1], index: i});
+    x.push(<ul key="1" className="collapsible">
+      <li>
+        <div className="collapsible-header">
+          <i className="material-icons">done</i>{word[0]}</div>
+        <div className="collapsible-body">
+          <span>{word[1]}</span>
+        </div>
+      </li>
+    </ul>)
+    this.setState({x: x, index: i});
   }
   previous = () => {
+    $('.collapsible').collapsible('close', 0);
+    var x = [];
     if (this.state.index > 0) {
       var i = this.state.index - 1;
       let word = this.props.data[i];
-      console.log(word)
       word = word.split(":");
-      this.setState({head: word[0], body: word[1], index: i});
+      x.push(<ul key="1" className="collapsible">
+        <li>
+          <div className="collapsible-header">
+            <i className="material-icons">done</i>{word[0]}</div>
+          <div className="collapsible-body">
+            <span>{word[1]}</span>
+          </div>
+        </li>
+      </ul>)
+      this.setState({x: x, index: i});
     }
   }
   render = () => {
-    var left={
-      'float':'left'
+    var left = {
+      'float': 'left'
     };
-    var right={'float':'right'};
+    var center = {
+      'textAlign': 'center'
+    };
+    var right = {
+      'float': 'right'
+    };
     return (<div className="container">
-      <ul className="collapsible">
-        <li>
-          <div className="collapsible-header">
-            <i className="material-icons">done</i>{this.state.head}</div>
-          <div className="collapsible-body">
-            <span>{this.state.body}</span>
-          </div>
-        </li>
-      </ul>
+      <div style={center}>{this.state.index}/{this.state.data.length}</div>
+      {this.state.x}
       <div className="row">
-        <div  style={left}>
+        <div style={left}>
           <a className="waves-effect waves-light btn" onClick={this.previous}>
             <i className="material-icons left">reply</i>button</a>
         </div>
-        <div   style={right}>
+        <div style={right}>
           <a className="waves-effect waves-light btn" onClick={this.next}>
             <i className="material-icons left">forward</i>button</a>
         </div>
